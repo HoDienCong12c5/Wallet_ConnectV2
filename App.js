b3063627020b3738015a7ed07aa07061ba4f09ab
cong@bacoor <cong@bacoor.co>
2023-01-06 17:29:03 +0700

    seconed

--- App.js
+++ App.js
@@ -2,17 +2,25 @@ import {StyleSheet, Text, View, Button} from 'react-native';
 import React from 'react';
 import {ethers, Wallet} from 'ethers';
 import crypto from 'crypto';
+const rpc = 'https://rpc.testnet.tomochain.com';
 const App = () => {
+  const createPrKey = () => {
+    const id = crypto.randomBytes(32).toString('hex');
+    return '0x' + id;
+  };
+  const prkey =
+    '0x9e07e78b0cc403fc5779b38288c191d9cf15dc7281e5c83a914f4cebb9e6c68d';
+  const pkey = '0x2fb6cF57d2B795b2b5838FcB641deaBeD265Ad13';
+
   const onPressLearnMore = async () => {
-    const provider = new ethers.providers.JsonRpcProvider(
-      'https://data-seed-prebsc-2-s3.binance.org:8545',
-    );
-    // const wallet = new Wallet(provider.getSigner())
-    var id = crypto.randomBytes(32).toString('hex');
-    var privateKey = '0x' + id;
-    console.log({privateKey});
-    var wallet = new ethers.Wallet(privateKey);
-    console.log({wallet});
+    const provider = new ethers.providers.JsonRpcProvider(rpc);
+
+    var wallet = new ethers.Wallet(prkey);
+    provider.getBalance(pkey).then(balance => {
+      // convert a currency unit from wei to ether
+      const balanceInEth = ethers.utils.formatEther(balance);
+      console.log(`balance: ${balanceInEth} ETH`);
+    });
     console.log('ok');
   };
   return (
